0815fec67fa8b298d481d334e6096fd8e6a362c4
Serge Kravchenko <0634380033@ukr.net>
2020-07-12 20:20:26 +0300

    Downloaded the first time the files

---
+++ Module-02-JS/HW-05/script.js
@@ -0,0 +1,462 @@
+// Task No. 01
+
+let a1 = 'helo';
+console.log (a1);
+document.writeln (a1 + "<br/>");
+
+let b1 = 'owu';
+console.log(b1);
+document.writeln (b1 + "<br/>");
+
+let c1 = 'com';
+console.log(c1);
+document.writeln (c1 + "<br/>");
+
+let d1 = 'ua';
+console.log(d1);
+document.writeln (d1 + "<br/>");
+
+let e1 = 1;
+console.log(e1);
+document.writeln (e1 + "<br/>");
+
+let f1 = 10;
+console.log(f1);
+document.writeln (f1 + "<br/>");
+
+let g1 = -999;
+console.log(g1);
+document.writeln  (g1 + "<br/>");
+
+let h1 = '123';
+console.log(h1);
+document.writeln (h1 + "<br/>");
+
+let i1 = 3.14;
+console.log(i1);
+document.writeln (i1 + "<br/>");
+
+let j1 = 2.7;
+console.log(j1);
+document.writeln (j1 + "<br/>");
+
+let k1 = 16;
+console.log(k1);
+document.writeln (k1 + "<br/>");
+
+let l1 = true;
+console.log(l1);
+document.writeln (l1 + "<br/>");
+
+let m1 = false;
+console.log(m1);
+document.writeln (m1 + "<br/>");
+
+// Task No. 02
+
+a1 = 1500;
+console.log (a1);
+document.writeln (a1 + "<br/>");
+//alert(a1);
+
+b1 = 'Wow';
+console.log(b1);
+document.writeln (b1 + "<br/>");
+//alert(b1);
+
+c1 = 454;
+console.log(c1);
+document.writeln (c1 + "<br/>");
+//alert(c1);
+
+d1 = 'asdfjkll';
+console.log(d1);
+document.writeln (d1 + "<br/>");
+//alert(d1);
+
+e1 = d1;
+console.log(e1);
+document.writeln (e1 + "<br/>");
+//alert(e1);
+
+f1 = 45454;
+console.log(f1);
+document.writeln (f1 + "<br/>");
+//alert(f1);
+
+g1 = 5;
+console.log(g1);
+document.writeln  (g1 + "<br/>");
+//alert(g1);
+
+h1 = 5485;
+console.log(h1);
+document.writeln (h1 + "<br/>");
+//alert(h1);
+
+i1 = 4545454545;
+console.log(i1);
+document.writeln (i1 + "<br/>");
+//alert(i1);
+
+j1 = 'ooo p s';
+console.log(j1);
+document.writeln (j1 + "<br/>");
+//alert(j1);
+
+k1 = 45487548;
+console.log(k1);
+document.writeln (k1 + "<br/>");
+//alert(k1);
+
+l1 = 'Super';
+console.log(l1);
+document.writeln (l1 + "<br/>");
+//alert(l1);
+
+m1 = 545105.54540;
+console.log(m1);
+document.writeln (m1 + "<br/>");
+//alert(m1);
+
+// Task No. 03
+
+const num1 = 42;
+const num2 = 87;
+const num3 = 4;
+
+console.log(num1, num2, num3);
+document.writeln(num1, ' ', num2, ' ', num3);
+
+const st1 = 'What`s up?';
+const st2 = 'О. Thank you for asking. Oh, it`s okay. ';
+const st3 = 'It`s great.';
+console.log(st1, st2, st3);
+document.writeln(st1, ' ', st2, ' ', st3);
+
+// Task No. 04
+
+//let username = prompt('Введите Имя');
+//let surname = prompt('Введите фамилию');
+//let patronymic = prompt('Введите отчество');
+//console.log(username + ' ' + surname  + ' ' + patronymic);
+//document.writeln(username + ' ' + surname  + ' ' + patronymic);
+
+// Task No. 05
+
+//let person = username + ' ' + surname  + ' ' + patronymic;
+//console.log(person);
+//document.writeln(person);
+
+// Task No. 06
+
+//let momName = prompt('Введи Имя мамы');
+//let momSurname = prompt('Введи фамилию мамы');
+//let momPatronymic = prompt('Введи отчество мамы');
+//console.log(momName + ' ' + momSurname + ' ' + momPatronymic);
+//document.writeln(momName + ' ' + momSurname + ' ' + momPatronymic);
+
+//let dadName = prompt('Введи Имя папы');
+//let dadSurname = prompt('Введи фамилию папы');
+//let dadPatronymic = prompt('Введи отчество папы');
+//console.log(dadName + ' ' + dadSurname + ' ' + dadPatronymic);
+//document.writeln(dadName + ' ' + dadSurname + ' ' + dadPatronymic);
+
+//let personMom = momName + ' ' + momSurname + ' ' + momPatronymic;
+//let personDad = dadName + ' ' + dadSurname + ' ' + dadPatronymic;
+//console.log(personMom + ' ' + personDad);
+//document.writeln(personMom + ' ' + personDad);
+
+// Task No. 07
+
+//let number1 = +prompt('Введи первое число');
+//let number2 = +prompt('Введи второе число');
+//let number3 = +prompt('Введи третье число');
+
+//console.log(number1);
+//console.log(number2);
+//console.log(number3);
+
+// Task No. 08
+
+//let enterN1 = prompt('Введите первое число ...');
+//let enterN2 = prompt('Введите второе число ...');
+//let enterN3 = prompt('Введите третье число ...');
+//let enterN4 = prompt('Введите четвертое число ...');
+
+//let result1 = parseInt(enterN1) + parseInt(enterN2) 
+//+ parseInt(enterN3) + parseInt(enterN4);
+//console.log(result1);
+//document.writeln(result1);
+
+// Task No. 09
+
+//let floatN1 = prompt('Введите первое число ...');
+//let floatN2 = prompt('Введите второе число ...');
+//let floatN3 = prompt('Введите третье число ...');
+
+//let resultFloat = parseFloat(floatN1) + parseFloat(floatN2) + parseFloat(floatN3);
+//console.log(resultFloat);
+//document.writeln(resultFloat);
+
+// Task No. 10
+
+//let floatN1 = +prompt('Enter Float number1');
+//let floatN2 = +prompt('Enter Float number2');
+//let floatN3 = +prompt('Enter Float number2');
+//resultFloat2 = Math.round(floatN1)+Math.round(floatN2)+Math.round(floatN3);
+
+//console.log(resultFloat2);
+//document.writeln(resultFloat2);
+
+// Task No. 11
+
+//let no01 = +prompt('Введите число, которое нужно возвести в степень ...');
+//let no02 = +prompt('В какую степерь вы хотите возвести число ...');
+//console.log(Math.pow(no01, no02));
+//document.writeln(Math.pow(no01, no02));
+
+// Task No. 12
+
+/*let a5 = 100;
+console.log(typeof a5);
+
+let b5 = '100';
+console.log(typeof b5);
+
+let c5 = true;
+console.log(typeof c5);
+
+let d5 = undefined;
+console.log(typeof d5);
+
+// Task No. 13
+
+console.log(5<6);
+console.log(5>6);
+console.log(5==6);
+console.log(5>=6);
+console.log(10==10);
+console.log(10===10);
+console.log(10<10);
+console.log(10>10);
+console.log(10!=10);
+console.log(123>123);
+console.log(123==123);
+
+// Task No. 14
+
+console.log(132 > 100 && 45 < 12 ); //false
+console.log(34 > 33 && 23 < 90 ); //true
+console.log(99 > 100 && 45 > 12 ); //false
+console.log(132 > 100 || 45 < 12 ); //true
+console.log(111 > 11 || 45 < 111 ); //true
+console.log((111 > 11 || 45 < 111) && (132 > 100 || 45 < 12) ); //true
+console.log((111 > 11 || 45 < 56) || (132 > 100 || 45 < 12) ); //true
+console.log((132 > 100 && 45 < 12 ) && (132 > 100 || 45 < 12 ) ); //false
+console.log((111 > 11 || 45 < 111) || (99 > 100 && 45 > 12 )); //true
+console.log(!!'-1'); //true
+console.log(!!-1); //true
+console.log(!!'0'); //true
+console.log(!!'null'); //true
+console.log(!!'undefined'); //true
+console.log(!!(3/'owu')); //false
+console.log((111 > 11 || 45 < 111) ||  !!'0'); //true
+console.log((!!111 == !!11 || 45 < 111) && (99 > 100 && 45 > 12 )); //false
+
+// Task No. 15
+
+let str = 'Привет';
+let num = 123;
+let flag = true;
+let txt = 'true';
+
+console.log(typeof str);
+console.log(typeof num);
+console.log(typeof flag);
+console.log(typeof txt);
+
+// Task No. 16
+
+let a1a = 5 + 3;
+let a2a = 5 - 3;
+let a3a = 5 * 3;
+let a4a = 5 / 3;
+let a5a = 5 % 3;
+
+console.log(a1a);
+console.log(a2a);
+console.log(a3a);
+console.log(a4a);
+console.log(a5a);
+
+// Task No. 17
+
+let a21 = 5 % 3;
+let a22 = 3 % 5;
+let a23 = 5 + '3';
+let a24 = '5' - 3;
+let a25 = 75 + 'кг';
+console.log(a21);
+console.log(a22);
+console.log(a23);
+console.log(a24);
+console.log(a25);
+
+// Task No. 18
+
+let height = 23;
+let width = 10;
+
+let s = height*width;
+console.log(s);
+
+// Task No. 19
+
+let heightC = 10;
+let dC = 4;
+let v = heightC*dC;
+console.log(v);
+
+// Task No. 20
+
+let side1 = 3;
+let side2= 4;
+let kq = Math.sqrt(Math.pow(side1,2)+Math.pow(side2,2));
+console.log(kq);
+
+// Task No. 21
+
+let str555 = 'Hello world';
+document.write(str555);
+console.log(str555);
+
+// Task No. 22
+
+let yourName = 'Serge \n';
+let yourSurname = 'Kravchenko \n';
+let yourFathersName = 'Vladimirovich \n';
+let yourAge = '39 \n';
+let yourHobby = 'Java \n';
+
+console.log (yourName + yourSurname + yourFathersName + yourAge + yourHobby);
+
+// Task No. 23
+
+let str1 = 'Кто ';
+let str2 = 'ты ';
+let str3 = 'такой?';
+let concatenation = (str1 + str2 + str3);
+document.write(concatenation);
+
+// Task No. 24
+
+let strz = "20";
+let az = 5;
+document.write(strz + az + "<br/>");
+document.write(strz - az + "<br/>");
+document.write(strz * "2" + "<br/>");
+document.write(strz / 2 + "<br/>");
+
+// Task No. 25
+
+console.log(parseInt("3.14"));
+console.log(parseInt("-7.875"));
+console.log(parseInt("435"));
+console.log(parseInt("Вася"));
+
+// Task No. 26
+
+//let strx = prompt("Enter something", "ho-ho")
+//console.log(strx);
+
+// Task No. 27
+
+//let t = +prompt('Enter number1');
+//let t1 = +prompt('enter number2');
+//alert(t+t1);
+
+// Task No. 28
+
+//let userNameX = prompt('Пожалуйста, введите свое имя ...');
+//let userSurnameX = prompt('А также фамилию ...');
+//let userAgeX = prompt('Кстати, какой у вас возраст ...');
+//alert('Добрый вечер' + ' ' + (userNameX + ' ' + userSurnameX)+(', рад за Вас,что вам ')+(userAgeX));
+
+// Task No. 29
+
+//let n1 = +prompt('Enter number1');
+//let n2 = +prompt('Enter number2');
+//let n3 = +prompt('Enter number3');
+
+/*if (n1<n2 && n2<n3){
+    console.log(n1,n2,n3)
+}
+else if (n1<n3 && n3<n2) {
+    console.log (n1,n3,n2);
+}
+else if (n2<n1 && n1<n3) {
+    console.log (n2,n1,n3);
+}
+else if (n2<n3 && n3<n1) {
+    console.log (n2,n3,n1);
+}
+else if (n3<n1 && n1<n2) {
+    console.log (n3,n1,n2);
+}
+else {
+    console.log (n3,n2,n1);
+}*/
+
+// Task No. 30
+/*
+let userColor = prompt('Введите номер цвета сфетофора 1 - красный, 2- зеленый, 3- желтый');
+switch (userColor) {
+    case '1':
+      document.write('Красный свет светофора. Нужно стоять и ждать.');
+      document.write('<br>');
+      break;
+  
+    case '2':
+      document.write('Зеленый свет светофора. Можно идти.');
+      document.write('<br>');
+      break;
+  
+    case '3':
+      document.write('Желтый свет светофора. Подожди немного.')
+      document.write('<br>');
+      break;
+  
+    default:
+      document.write('Что то не то ты ввел. Попробуй еще раз.')
+      document.write('<br>');
+      break;
+  }*/
+
+  // Task No. 31
+
+  let isRoadClear = confirm('Машины есть?');
+    console.log(isRoadClear);
+
+let colour = prompt('Введите цвет светофора6 зеленый, желтый или красный?');
+if (colour === 'зеленый' && isRoadClear) {
+    console.log('иди!');
+}
+else if (colour === 'зеленый' && !isRoadClear) {
+    console.log('подожди пока нарушители проедут');
+}
+else if (colour === 'желтый' && isRoadClear) {
+    console.log('все равно жди');
+}
+else if (colour === 'желтый' && !isRoadClear) {
+    console.log('жди');
+}
+else if (colour === 'красный' && isRoadClear) {
+    console.log('стой все равно');
+}
+else if (colour === 'красный' && !isRoadClear) {
+    console.log('стой и жди');
+}
+else {
+    console.log('делай что хочешь');
+}
