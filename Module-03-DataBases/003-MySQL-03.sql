299a88fda690ce1091a53ec69be5e531b5678682
Serge Kravchenko <55243765+LeeTovetz@users.noreply.github.com>
2020-09-28 20:58:05 +0300

    Update 003-MySQL-03.sql

--- Module-03-DataBases/003-MySQL-03.sql
+++ Module-03-DataBases/003-MySQL-03.sql
@@ -150,3 +150,4 @@ SELECT City FROM(
 SELECT *,Sum(Sum) AS suma,Client_idClient AS id FROM applicatiON
 JOIN client c ON applicatiON.Client_idClient = c.idClient
 GROUP BY Client_idClient ORDER BY suma desc limit 1) AS t WHERE id=idClient;
+
